2082bb0fe574ee46d7d0bdbff62d30a96d4358e4
Mark <mark.kenny@yandex.ru>
2022-05-04 18:10:28 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,107 @@
+import socket
+import threading
+# tkinter - интерфейсная библиотека
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+
+# наш клиент будет не просто скриптом, как модуль server, а будет отдельным классом, фактически объектом Python
+class Client:
+
+    # определяем метод инициализации и передаем всю поддержку (HOST & PORT)
+    def __init__(self, host, port):
+
+        # определяем клиентский сокет(сервер) и подключаемся к порту сервера
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        self.tk_chat_window = None
+
+        # Открываем всплывающее окно, чтобы пользователь мог ввести имя пользователя, т.е. свой никнэйм
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        # Спрашиваем что-нибудь в диалоговом окне и результатом будет псевдоним, т.е. никнэйм
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
+
+
+        self.running = True
+
+        # Запускаем два потока (цикл графического интерфейса и поток приема)
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    # Функция для создания графического интерфейса
+    def gui_loop(self):
+        self.tk_chat_window = tkinter.Tk()
+        self.tk_chat_window.configure(bg="lightgray")
+
+        self.chat_lable = tkinter.Label(self.tk_chat_window, text="Chat:", bg="lightgray")
+        self.chat_lable.config(font=("Arial", 12))
+        self.chat_lable.pack(padx=25, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.tk_chat_window)
+        self.text_area.pack(padx=25, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_lable = tkinter.Label(self.tk_chat_window, text="Message:", bg="lightgray")
+        self.msg_lable.config(font=("Arial", 12))
+        self.msg_lable.pack(padx=25, pady=5)
+
+        self.input_area = tkinter.Text(self.tk_chat_window, height=5)
+        self.input_area.pack(padx=25, pady=5)
+
+        # Создаём кнопку
+        self.send_button = tkinter.Button(self.tk_chat_window, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=25, ipadx=5)
+
+        self.tk_chat_window.mainloop()
+
+        # Когда пользователь закрывает окно (завершается программа)
+        self.tk_chat_window.protocol("WM_DELETE_WINDOW", self.stop)
+
+    def write(self):
+        # get('1.0', 'end') - означает получение всего текста
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    # Функция остановки
+    def stop(self):
+        self.running = False
+        self.tk_chat_window.destroy()
+        self.sock.close()
+        exit(0)
+
+
+    # Функция обрабатывает новые соединения, мы получаем новые сообщения от сервера
+    def receive(self):
+        while self.running:
+            try:
+                # пытаемся получить сообщение от сервера
+                message = self.sock.recv(1024)
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.tk_chat_window:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disable')
+
+            except ConnectionAbortedError:
+                break
+            except:
+                print('Error')
+                self.sock.close()
+                break
+
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+client = Client(HOST, PORT)
---
+++ server.py
@@ -0,0 +1,71 @@
+import socket
+import threading
+"""Cоздает многопоточность (мы хотим, чтобы много разных потоков выполнялось одновременно)
+хотим получать новые соединения, одновременно обрабатывать 10 клиентских соединений """
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+
+# broadcast - широковещательная функция, которая отправляет одно сообщение всем подключенным пользователям
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+# handle - функция обработки, обрабатывает отдельные соединения с клиентом
+def handle(client):
+    while True:
+        try:
+            # recv(1024) - пытаемся получить сообщение от клиента
+            message = client.recv(1024)
+            print(f'{nicknames[clients.index(client)]} says {message}')
+            broadcast(message)
+        except:
+            # Если мы получим ошибку (м.б. Клиент просто отключился или вышел из строя) мы удаляем его
+            # из списка псевдонимов (nickname) и из списка клиентов и выйдем из цикла
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+# receive - функция, которая принимает новые соединения и слушает, пока новый клиент не подключится
+def receive():
+    while True:
+        # accept() возвращает адрес клиента и сервера, а затем мы принимаем соединение и у нас есть новый клиент
+        client, address = server.accept()
+        print(f'Новый пользователь: {client}!')
+        print(f'Новый адрес: {address}!')
+        # просим клиента ввести никнэйм и принимаем сообщение при помощи метода recv()
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} connected to the server!\n'.encode('utf-8'))
+        client.send('Connected to the server'.encode('utf-8'))
+
+        #
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server running...")
+# Вызываем метод приёма, он в свою очередь вызывает метод обработки handle, а handle вызывает broadcast
+# широковещательный метод
+receive()
+
+
